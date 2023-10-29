e6c76378cdd28a57e557b12e10e38180e287917f
fromtimbuktuwithlove@gmail.com <fromtimbuktuwithlove@gmail.com>
2023-10-29 18:20:16 -0500

    initial commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,29 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+
+    {
+        title: 'Services',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Products',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Shop Now',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
